867bd014da03e463db8401138d36f3eb116aa9fb
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-12-09 22:45:40 +0100

    cleaned code

--- 1/main.py
+++ 1/main.py
@@ -7,21 +7,15 @@ def main():
 
     import os
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.txt"), "r+") as file:
-        inp = file.readline()
-        while inp != "":
-            left, right = map(int, inp.split("   "))
+        for line in file:
+            left, right = map(int, line.split("   "))
             left_list.append(left)
             right_list.append(right)
 
-            inp = file.readline()
-
-    left_list.sort()
-    right_list.sort()
-
     summation: float = 0
 
-    while (len(left_list), len(right_list)) != (0, 0):
-        summation += ((left_list.pop() - right_list.pop())**2)**0.5
+    for left, right in zip(sorted(left_list), sorted(right_list)):
+        summation += abs(left - right)
 
     print(summation)
     print(summation == 2367773)
