191d3f8d50d0398366d2d4f82e2a7746b6b85776
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-12-09 23:13:47 +0100

    readability

--- 2/main.py
+++ 2/main.py
@@ -8,16 +8,20 @@ def main():
         for line in file:
             report: list[int] = [int(i) for i in line.split()]
 
-            safe: bool = True
-            if (len(set(report)) == len(report) and
-                    report in [sorted(report), sorted(report, reverse=True)]):
-                for index in range(0, len(report) - 1):
-                    if abs(report[index] - report[index + 1]) not in [1, 2 ,3]:
-                        safe = False
-                        break
+            if len(set(report)) != len(report):
+                continue
 
-                if safe:
-                    sum_safe += 1
+            if report not in [sorted(report), sorted(report, reverse=True)]:
+                continue
+
+            safe: bool = True
+            for index in range(0, len(report) - 1):
+                if abs(report[index] - report[index + 1]) not in [1, 2 ,3]:
+                    safe = False
+                    break
+
+            if safe:
+                sum_safe += 1
 
     print(sum_safe)
     print(sum_safe == 479)
