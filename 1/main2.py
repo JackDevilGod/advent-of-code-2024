84d730b0873192efb98817e227bf0ea257294e01
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-12-09 22:49:07 +0100

    optimizations

--- 1/main2.py
+++ 1/main2.py
@@ -1,25 +1,26 @@
+from time import perf_counter
+
+
 def main():
-    inp = input()
     left_list: list = []
     right_list: list = []
 
-    while inp != "":
-        left, right = map(int, inp.split("   "))
-        left_list.append(left)
-        right_list.append(right)
-
-        inp = input()
-
-    left_list.sort()
-    right_list.sort()
+    import os
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.txt"), "r+") as file:
+        for line in file:
+            left, right = map(int, line.split("   "))
+            left_list.append(left)
+            right_list.append(right)
 
     summation: float = 0
-
-    for num in left_list:
+    for num in sorted(left_list):
         summation += num * right_list.count(num)
 
     print(summation)
+    print(summation == 21271939)
 
 
 if __name__ == '__main__':
+    start = perf_counter()
     main()
+    print(perf_counter() - start)
