6e37226099082abd072c0ae1e0134b19d92e3f1d
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-12-16 15:24:27 +0100

    inter

--- 16/main2.py
+++ 16/main2.py
@@ -24,54 +24,54 @@ def main():
     distance_start_finish: float = (((abs(starting_point[0] - end_point[0])) +
                                     (abs(starting_point[1] - end_point[1]))))
 
-    move_queue: list[tuple[tuple[int, int], str, int, set]] = [(starting_point, "E",  0,
+    end_points: int | float = float("inf")
+    end_tiles: set = set()
+    move_queue: list[tuple[tuple[int, int], str, int, set]] = [(starting_point,
+                                                                "E",
+                                                                0,
                                                                 {starting_point})]
-    tiles_best_paths: set = set()
-    best_score: int | float = float("inf")
+    explored: set[tuple[int, int]] = set()
     while len(move_queue) > 0:
         current_position, direction, current_score, path = move_queue.pop(0)
         current_x, current_y = current_position
+        explored.add((current_position))
         print(current_position)
 
-        if (matrix[current_y + 1][current_x] == "." and direction != "S" and
-                (current_x, current_y + 1) not in path):
+        if matrix[current_y + 1][current_x] == "." and direction != "S":
             if direction == "N":
-                move_queue.append(((current_x, current_y + 1), "N",
-                                   current_score + 1, path.union({(current_x, current_y + 1)})))
+                move_queue.append(((current_x, current_y + 1), "N", current_score + 1,
+                                   path.union({(current_x, current_y + 1)})))
             else:
-                move_queue.append(((current_x, current_y + 1), "N",
-                                   current_score + 1001,
+                move_queue.append(((current_x, current_y + 1), "N", current_score + 1001,
                                    path.union({(current_x, current_y + 1)})))
         elif matrix[current_y + 1][current_x] == "E" and direction != "S":
-            if direction == "N" and current_score + 1 <= best_score:
-                tiles_best_paths.update(path.union({(current_x, current_y + 1)}))
-                best_score = current_score + 1
-            elif current_score + 1001 <= best_score:
-                best_score = current_score + 1001
-                tiles_best_paths.update(path.union({(current_x, current_y + 1)}))
+            if direction == "N":
+                if current_score + 1 <= end_points:
+                    end_points = current_score + 1
+                    end_tiles.update(path.union({(current_x, current_y + 1)}))
+            else:
+                if current_score + 1001 <= end_points:
+                    end_points = current_score + 1001
+                    end_tiles.update(path.union({(current_x, current_y + 1)}))
 
-        if (matrix[current_y][current_x + 1] == "." and direction != "W" and
-                (current_x + 1, current_y) not in path):
+        if matrix[current_y][current_x + 1] == "." and direction != "W":
             if direction == "E":
-                move_queue.append(((current_x + 1, current_y),
-                                   "E",
-                                   current_score + 1,
+                move_queue.append(((current_x + 1, current_y), "E", current_score + 1,
                                    path.union({(current_x + 1, current_y)})))
             else:
-                move_queue.append(((current_x + 1, current_y),
-                                   "E",
-                                   current_score + 1001,
+                move_queue.append(((current_x + 1, current_y), "E", current_score + 1001,
                                    path.union({(current_x + 1, current_y)})))
         elif matrix[current_y][current_x + 1] == "E" and direction != "W":
-            if direction == "E" and current_score + 1 <= best_score:
-                tiles_best_paths.update(path.union({(current_x + 1, current_y)}))
-                best_score = current_score + 1
-            elif current_score + 1001 <= best_score:
-                best_score = current_score + 1001
-                tiles_best_paths.update(path.union({(current_x + 1, current_y)}))
+            if direction == "E":
+                if current_score + 1 <= end_points:
+                    end_points = current_score + 1
+                    end_tiles.update(path.union({(current_x + 1, current_y)}))
+            else:
+                if current_score + 1001 <= end_points:
+                    end_points = current_score + 1001
+                    end_tiles.update(path.union({(current_x + 1, current_y)}))
 
-        if (matrix[current_y - 1][current_x] == "." and direction != "N" and
-                (current_x, current_y - 1) not in path):
+        if matrix[current_y - 1][current_x] == "." and direction != "N":
             if direction == "S":
                 move_queue.append(((current_x, current_y - 1), "S", current_score + 1,
                                    path.union({(current_x, current_y - 1)})))
@@ -79,15 +79,16 @@ def main():
                 move_queue.append(((current_x, current_y - 1), "S", current_score + 1001,
                                    path.union({(current_x, current_y - 1)})))
         elif matrix[current_y - 1][current_x] == "E" and direction != "N":
-            if direction == "S" and current_score + 1 <= best_score:
-                tiles_best_paths.update(path.union({(current_x, current_y - 1)}))
-                best_score = current_score + 1
-            elif current_score + 1001 <= best_score:
-                best_score = current_score + 1001
-                tiles_best_paths.update(path.union({(current_x, current_y - 1)}))
+            if direction == "S":
+                if current_score + 1 <= end_points:
+                    end_points = current_score + 1
+                    end_tiles.update(path.union({(current_x, current_y - 1)}))
+            else:
+                if current_score + 1001 <= end_points:
+                    end_points = current_score + 1001
+                    end_tiles.update(path.union({(current_x, current_y - 1)}))
 
-        if (matrix[current_y][current_x - 1] == "." and direction != "E" and
-                (current_x - 1, current_y) not in path):
+        if matrix[current_y][current_x - 1] == "." and direction != "E":
             if direction == "W":
                 move_queue.append(((current_x - 1, current_y), "W", current_score + 1,
                                    path.union({(current_x - 1, current_y)})))
@@ -95,19 +96,21 @@ def main():
                 move_queue.append(((current_x - 1, current_y), "W", current_score + 1001,
                                    path.union({(current_x - 1, current_y)})))
         elif matrix[current_y][current_x - 1] == "E" and direction != "E":
-            if direction == "W" and current_score + 1 <= best_score:
-                tiles_best_paths.update(path.union({(current_x - 1, current_y)}))
-                best_score = current_score + 1
-            elif current_score + 1001 <= best_score:
-                best_score = current_score + 1001
-                tiles_best_paths.update(path.union({(current_x - 1, current_y)}))
+            if direction == "N":
+                if current_score + 1 <= end_points:
+                    end_points = current_score + 1
+                    end_tiles.update(path.union({(current_x - 1, current_y)}))
+            else:
+                if current_score + 1001 <= end_points:
+                    end_points = current_score + 1001
+                    end_tiles.update(path.union({(current_x - 1, current_y)}))
 
-        move_queue = sorted(move_queue,
+        move_queue = sorted(filter(lambda x: x[2] < end_points,
+                                   move_queue),
                             key=lambda x: ((x[2] / distance_start_finish) +
-                                           abs(x[0][0] - end_point[0]) +
-                                           abs(x[0][1] - end_point[1])))
+                            abs(x[0][0] - end_point[0]) + abs(x[0][1] - end_point[1])))
 
-    print(len(tiles_best_paths))
+    print(len(end_tiles))
 
 
 if __name__ == '__main__':
