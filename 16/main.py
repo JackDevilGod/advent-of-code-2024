297e69b4997d6265e39b06696ccd370e234b6f80
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-12-16 20:35:15 +0100

    made a more readable version

--- 16/main.py
+++ 16/main.py
@@ -25,61 +25,37 @@ def main():
                                     (abs(starting_point[1] - end_point[1]))))
 
     end_points: int = 0
-    move_queue: list[tuple[tuple[int, int], str, int]] = [(starting_point, "E",  0)]
-    explored: set[tuple[tuple[int, int], str, int]] = set()
+    move_queue: list[tuple[tuple[int, int], tuple[int, int], int]] = [(starting_point, (1, 0),  0)]
+    explored: set[tuple[int, int]] = set()
     while True:
         current_position, direction, current_score = move_queue.pop(0)
         current_x, current_y = current_position
-        explored.add((current_position, direction, current_score))
+        explored.add(current_position)
         print(current_position)
 
-        if matrix[current_y + 1][current_x] == "." and direction != "S":
-            if direction == "N":
-                move_queue.append(((current_x, current_y + 1), "N", current_score + 1))
-            else:
-                move_queue.append(((current_x, current_y + 1), "N", current_score + 1001))
-        elif matrix[current_y + 1][current_x] == "E" and direction != "S":
-            if direction == "N":
-                end_points = current_score + 1
-            else:
-                end_points = current_score + 1001
+        if current_position == end_point:
+            end_points = current_score
             break
 
-        if matrix[current_y][current_x + 1] == "." and direction != "W":
-            if direction == "E":
-                move_queue.append(((current_x + 1, current_y), "E", current_score + 1))
-            else:
-                move_queue.append(((current_x + 1, current_y), "E", current_score + 1001))
-        elif matrix[current_y][current_x + 1] == "E" and direction != "W":
-            if direction == "E":
-                end_points = current_score + 1
-            else:
-                end_points = current_score + 1001
-            break
+        up_offset: tuple[int, int] = (current_x, current_y - 1)
+        if matrix[up_offset[1]][up_offset[0]] != "#" and up_offset not in explored:
+            move_queue.append((up_offset, (0, -1),
+                               current_score + abs(1001 * direction[0]) + abs(1 * direction[1])))
 
-        if matrix[current_y - 1][current_x] == "." and direction != "N":
-            if direction == "S":
-                move_queue.append(((current_x, current_y - 1), "S", current_score + 1))
-            else:
-                move_queue.append(((current_x, current_y - 1), "S", current_score + 1001))
-        elif matrix[current_y - 1][current_x] == "E" and direction != "N":
-            if direction == "S":
-                end_points = current_score + 1
-            else:
-                end_points = current_score + 1001
-            break
+        down_offset: tuple[int, int] = (current_x, current_y + 1)
+        if matrix[down_offset[1]][down_offset[0]] != "#" and down_offset not in explored:
+            move_queue.append((down_offset, (0, 1),
+                               current_score + abs(1001 * direction[0]) + abs(1 * direction[1])))
 
-        if matrix[current_y][current_x - 1] == "." and direction != "E":
-            if direction == "W":
-                move_queue.append(((current_x - 1, current_y), "W", current_score + 1))
-            else:
-                move_queue.append(((current_x - 1, current_y), "W", current_score + 1001))
-        elif matrix[current_y][current_x - 1] == "E" and direction != "E":
-            if direction == "W":
-                end_points = current_score + 1
-            else:
-                end_points = current_score + 1001
-            break
+        right_offset: tuple[int, int] = (current_x + 1, current_y)
+        if matrix[right_offset[1]][right_offset[0]] != "#" and right_offset not in explored:
+            move_queue.append((right_offset, (1, 0),
+                               current_score + abs(1001 * direction[1]) + abs(1 * direction[0])))
+
+        left_offset: tuple[int, int] = (current_x - 1, current_y)
+        if matrix[left_offset[1]][left_offset[0]] != "#" and left_offset not in explored:
+            move_queue.append((left_offset, (-1, 0),
+                               current_score + abs(1001 * direction[1]) + abs(1 * direction[0])))
 
         move_queue = sorted(list(set(move_queue).difference(explored)),
                             key=lambda x: ((x[2] / distance_start_finish) +
